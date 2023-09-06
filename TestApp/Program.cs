3817752f5540e72559e6065cdff70ee9931a1655
trinhdinhvu2312 <dragonking023@gmail.com>
2023-09-06 17:18:23 +0700

    update

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TestApp.Models;
+using TestApp.Controllers;
+using System.Net;
+using TestApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +12,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer2017")));
+builder.Services.AddScoped<DataSeeder>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+//builder.WebHost.UseUrls("http://127.0.0.1:5000");
+
+builder.WebHost.ConfigureKestrel(kestrelServerOptions =>
+{
+    kestrelServerOptions.Listen(IPAddress.Loopback, 5002);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,9 +43,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
